1ba1505d2d71b7703d3deef9742fec9ff2c50a9a
jpsandoval <juampiboy@gmail.com>
2022-10-16 20:38:07 -0300

    adding descriptions

--- creational-patterns/abstract-factory/pizza-factory/pizza_store.rb
+++ creational-patterns/abstract-factory/pizza-factory/pizza_store.rb
@@ -8,6 +8,10 @@ class PizzaStore
     pizza.box
     pizza
   end
+  # este es un abstract factory method
+  # un metodo que crea objetos y es abstract
+  # lo definen las clases hija
+  # cada clase hija se encarga de crear una familia de objetos
   def create(type)
     raise NotImplementedError
   end
